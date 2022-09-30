8583eaf4d975cae7f31ea4c7d35f87312bebd3af
OwenBokovnev <ebokovnev@gmail.com>
2022-09-30 17:23:05 +0300

    Task Descision

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter words AND/OR numbers with SPACE: ");
+string userText = Convert.ToString(Console.ReadLine());
+string[] words = userText.Split(' ');
+
+foreach (var word in words)
+{
+    if (word.Length <= 3)
+    {
+        Console.Write($"{word}" + ", ");    
+    }
+}
+Console.WriteLine("\n");
